cd4d74405a65a57d0bf094d5839feffe0352fac0
Frank Ackermann <acki77.github@digital-filestore.de>
2025-12-16 23:28:09 +0100

    update switch button uper lower case

--- js/js-input-and-strings/js-inputs-and-strings_switch-values/js/index.js
+++ js/js-input-and-strings/js-inputs-and-strings_switch-values/js/index.js
@@ -3,6 +3,9 @@ console.clear();
 const firstInput = document.querySelector('[data-js="first-input"]');
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
+const switchcaseButton = document.querySelector(
+  '[data-js="button-switchcase"]'
+);
 
 uppercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toUpperCase();
@@ -10,4 +13,7 @@ uppercaseButton.addEventListener("click", () => {
 
 lowercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toLowerCase();
+});
+switchcaseButton.addEventListener("click", () => {
+  // think about with prepare true false
 });
