70f3fced45861588bc038dc81e189a5abfe27992
Frank Ackermann <acki77.github@digital-filestore.de>
2025-12-18 23:39:28 +0100

    solved social post

--- js/js-create-elements/js-createelement_social-media-post/js/index.js
+++ js/js-create-elements/js-createelement_social-media-post/js/index.js
@@ -10,3 +10,25 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+const article = document.createElement("article");
+const content = document.createElement("p");
+const footer = document.createElement("footer");
+const username = document.createElement("span");
+const addedLikeButton = document.createElement("button");
+
+article.classList.add("post");
+content.classList.add("post__content");
+footer.classList.add("post__footer");
+username.classList.add("post__username");
+addedLikeButton.classList.add("post__button");
+
+content.textContent =
+  "Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
+username.textContent = "@username";
+addedLikeButton.textContent = "♥ Like";
+
+addedLikeButton.addEventListener("click", handleLikeButtonClick);
+
+article.append(content, footer);
+footer.append(username, addedLikeButton);
+document.body.append(article);
