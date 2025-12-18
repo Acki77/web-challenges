4180bcfa57ee5ac9d6918c360b64b61938cc7d51
Frank Ackermann <acki77.github@digital-filestore.de>
2025-12-18 14:12:56 +0100

    solved js-forms-submit

--- js/js-forms/js-forms_handle-form-submit/js/index.js
+++ js/js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,14 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log("EventTarget", event.target);
+  const formdata = new FormData(event.target);
+  const data = Object.fromEntries(formdata);
+
+  console.log("data", data);
+  event.target.reset();
+  event.target.elements.firstName.focus();
+});
