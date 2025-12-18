4563c932f385e1391954b4464c158a2137a8bcfc
Frank Ackermann <acki77.github@digital-filestore.de>
2025-12-18 16:25:39 +0100

    solved challenge toast message button

--- js/js-create-elements/js-createelement_toast-messages/js/index.js
+++ js/js-create-elements/js-createelement_toast-messages/js/index.js
@@ -4,10 +4,16 @@ const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
-addButton.addEventListener("click", () => {
+addButton.addEventListener("click", (event) => {
   // Exercise: Append a new entry to the toast messages container
+  event.preventDefault();
+  const li = document.createElement("li");
+  li.textContent = "I'm a toast message again!";
+  li.classList.add("toast-container__message");
+  toastContainer.append(li);
 });
 
 clearButton.addEventListener("click", () => {
+  toastContainer.innerHTML = "";
   // Exercise: Clear the stack of toast messages
 });
