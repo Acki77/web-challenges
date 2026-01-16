87138357252cb66f61647278041fe8d35c22cbad
Frank Ackermann <acki77.github@digital-filestore.de>
2026-01-16 15:53:11 +0100

    solved react-props- greeting challenge with array and map

--- react/react-props/react-props_greeting/src/App.jsx
+++ react/react-props/react-props_greeting/src/App.jsx
@@ -1,3 +1,17 @@
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  const names = ["Falk", "Ralf", "Felix", "Klaus"];
+  return (
+    <>
+      {names.map((singleName) => (
+        <Greeting key={singleName} name={singleName} />
+      ))}
+    </>
+  );
+}
+function Greeting({ name }) {
+  const coaches = ["Felix", "Roland", "Klaus"];
+  if (coaches.includes(name)) {
+    return <h1>Hello, Coach {name}!</h1>;
+  }
+  return <h1>Hello, {name}!</h1>;
 }
