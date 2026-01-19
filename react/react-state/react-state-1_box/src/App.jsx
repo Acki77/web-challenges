bd5d073de781b93feeb07455537aadcb18b7da92
Frank Ackermann <acki77.github@digital-filestore.de>
2026-01-19 20:08:26 +0100

    solved react-state1-box

--- react/react-state/react-state-1_box/src/App.jsx
+++ react/react-state/react-state-1_box/src/App.jsx
@@ -1,18 +1,21 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
-  let isActive = false;
+  const [isActive, setIsActived] = useState(false);
 
   function handleClick() {
-    isActive = !isActive;
+    setIsActived(!isActive);
     // Check that the value changes correctly.
-    console.log(isActive);
+    console.log(!isActive);
   }
 
   return (
     <main>
       <div className={`box ${isActive ? "box--active" : ""}`} />
-      <button onClick={handleClick}>Activate</button>
+      <button
+        onClick={handleClick}
+      >{`${isActive ? "Disactivate" : "Activate"}`}</button>
     </main>
   );
 }
