d86f4384549d019ccbb1cf4f2cfea8755a80eeb2
Frank Ackermann <acki77.github@digital-filestore.de>
2026-01-19 20:49:03 +0100

    solved react state1 counter

--- react/react-state/react-state-1_counter/src/App.jsx
+++ react/react-state/react-state-1_counter/src/App.jsx
@@ -1,23 +1,27 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
-  let count = 0;
+  const [count, setCount] = useState(0);
+
+  function CounterPlus() {
+    setCount(count + 1);
+  }
+  function CounterMinus() {
+    setCount(count - 1);
+  }
 
   return (
     <div className="container">
       <h1>{count}</h1>
       <div className="button-container">
-        <button
-          type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
-        >
+        <button type="button" onClick={CounterMinus}>
           -
         </button>
         <button
           type="button"
           onClick={() => {
+            CounterPlus();
             console.log("🤔");
           }}
         >
