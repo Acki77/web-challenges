2b21a01a82d041645ee4ab73d9a77acbde2234d2
Frank Ackermann <acki77.github@digital-filestore.de>
2026-01-19 10:34:30 +0100

    solved nesting buttons

--- react/react-nesting/react-nesting_buttons-and-children/src/App.jsx
+++ react/react-nesting/react-nesting_buttons-and-children/src/App.jsx
@@ -4,18 +4,24 @@ import "./styles.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button color="green" tooltipText="Daten sichern">
+        button 1
+      </Button>
+      <Button>button 2</Button>
+      <Button>button 3</Button>
+      <Button>button 4</Button>
     </main>
   );
 }
 
-function Button() {
+function Button({ children, color, tooltipText }) {
   return (
-    <button className="button" type="button">
-      Click me!
+    <button
+      className="button"
+      style={{ backgroundColor: color }}
+      title={tooltipText}
+    >
+      {children}
     </button>
   );
 }
