3f7803138e37427f89f5914472b762650af72a6b
Frank Ackermann <acki77.github@digital-filestore.de>
2026-01-22 01:19:53 +0100

    solved colored numbers and create snipped for react like useState import, console log

--- react/react-state2/react-state-2_colored-number/src/components/Counter.jsx
+++ react/react-state2/react-state-2_colored-number/src/components/Counter.jsx
@@ -1,15 +1,26 @@
 import ColoredNumber from "./ColoredNumber";
-
+import { useState } from "react";
 export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  function incrementCount() {
+    setCount(count + 1);
+  }
+
+  function decrementCount() {
+    setCount(count - 1);
+    console.log("🤔 count:", count);
+  }
   return (
     <div className="counter">
       <h1>Colored Number</h1>
-      <ColoredNumber value={0} />
+      <ColoredNumber value={count} />
       <div className="counter__buttons-container">
         <button
           type="button"
           className="counter__button"
           aria-label="increment count"
+          onClick={incrementCount}
         >
           +
         </button>
@@ -17,6 +28,7 @@ export default function Counter() {
           type="button"
           className="counter__button"
           aria-label="decrement count"
+          onClick={decrementCount}
         >
           -
         </button>
