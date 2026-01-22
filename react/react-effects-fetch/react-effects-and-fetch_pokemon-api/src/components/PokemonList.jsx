6bc6a4f2e6cd5875fdb6d8c7b935b1b0f955c247
Frank Ackermann <acki77.github@digital-filestore.de>
2026-01-22 15:07:13 +0100

    update solving effect pokemon api

--- react/react-effects-fetch/react-effects-and-fetch_pokemon-api/src/components/PokemonList.jsx
+++ react/react-effects-fetch/react-effects-and-fetch_pokemon-api/src/components/PokemonList.jsx
@@ -1,21 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
+  let [reload, setReload] = useState(0);
 
-  async function loadPokemon() {
-    try {
-      const response = await fetch("https://pokeapi.co/api/v2/pokemon");
-      const data = await response.json();
-      setPokemon(data.results);
-    } catch (error) {
-      console.log(error);
+  useEffect(() => {
+    console.log("🤔 reload:", reload);
+
+    async function loadPokemon() {
+      try {
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+        const data = await response.json();
+        setPokemon(data.results);
+      } catch (error) {
+        console.log(error);
+      }
     }
-  }
-
+    loadPokemon();
+  }, [reload]);
   return (
     <main>
-      <button type="button" onClick={loadPokemon}>
+      <button type="button" onClick={() => setReload(reload + 1)}>
         Load Pokémon
       </button>
       <ul>
